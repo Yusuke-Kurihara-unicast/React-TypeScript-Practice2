4ee3ffa98abd37e3b0d85fc6e4b3f86a5d2dcd99
Yusuke-Kurihara-unicast <91720261+Yusuke-Kurihara-unicast@users.noreply.github.com>
2021-10-25 18:18:37 +0900

    型定義のお勉強終了

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import "./styles.css";
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
 import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
+
+const user: User = {
+  name: "康夫",
+  hobbies: ["焚書", "異端審問"]
+};
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -17,6 +24,8 @@ export default function App() {
   };
   return (
     <div className="App">
+      {console.log("aaaa")}
+      <UserProfile user={user} />
       <Text color="#19999e" fontSize="40px" />
       <button onClick={onClickFetchData}>データを取得する</button>
       {todos.map((todo) => (
